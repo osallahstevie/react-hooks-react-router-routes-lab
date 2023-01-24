2a45f87f14272ad9bb07c141c6c93680019f5f16
osallahstevie <osallahstevie@gmail.com>
2023-01-24 17:28:34 +0300

    finished rect router routes lab

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,27 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      {
+        actors.map((actor, index) => {
+            return <div key={index}>
+              <h2>Name: {actor.name}</h2>
+              <p>Movies:</p>
+              <ul>
+                {
+                  actor.movies.map((movie, index) => {
+                    return <li key={index}>{movie}</li>
+                  })
+                }
+              </ul>
+            </div>
+          }
+        )
+      }
+    </div>
+  )
 }
 
-export default Actors;
+export default Actors
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,32 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {
+        movies.map((movie, index) => {
+          return (
+            <div key={index}>
+              <h2>Name: {movie.title}</h2>
+              <p>Time: {movie.time}</p>
+              <p>Genres:</p>
+              <ul>
+              {
+                movie.genres.map((genre, index) => {
+                  return (
+                    <li key={index}>{genre}</li>
+                  )
+                })
+              }
+              </ul>
+            </div>
+            )
+          }
+        )
+      }
+    </div>
+  );
 }
 
-export default Movies;
+export default Movies
